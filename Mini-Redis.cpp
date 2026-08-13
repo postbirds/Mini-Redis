7486d259f72f42f11c27e15457a7015d021f823a
KSY <sye.g112@gmail.com>
2026-08-13 12:25:46 +0900

    feat: KVDatabase Mutex 이용. 읽기 쓰기 락

--- Mini-Redis.cpp
+++ Mini-Redis.cpp
@@ -1,15 +1,32 @@
-﻿// Mini-Redis.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include "KVDatabase.h"
 
-#include <iostream>
+void threadTask(KVDatabase& db, int thread_id) { 
+	std::string key = "user_" + std::to_string(thread_id);
+
+	db.set(key, "data");
+
+	std::cout << "[Thread " << thread_id << "] GET " << key << ": " << db.get(key) << "\n";
+}
+
 
 int main()
 {
 	std::cout << "=== Mini-Redis 서버 시작 ===\n";
+	KVDatabase db;
+	std::vector<std::thread> threads;
 
-	
-	std::cout << "Get name: ";
-	std::cout << "Get age: ";
+	for (int i = 0; i < 10; i++) {
+		threads.emplace_back(threadTask, std::ref(db), i);
+	}
+
+	for (auto& t : threads) {
+		t.join();
+	}
+		
+	std::cout << "=== 끝 == \n";
 
 	return 0;
 }
