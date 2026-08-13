9375e7797a976a4878fb3813544e49424611fee8
KSY <sye.g112@gmail.com>
2026-08-13 11:28:51 +0900

    feat: KVDatabase 코어 엔진 및 GET/SET 기능 구현

--- KVDatabase.cpp
+++ KVDatabase.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 
 void KVDatabase::set(const std::string& key, const std::string& value) {
-	
+	store[key] = value;
+	std::cout << "[SET] " << key << " = " << value << "\n";
 }
 
 std::string KVDatabase::get(const std::string& key) {
-
+	if(store.find(key) != store.end()){
+		return store[key];
+	}
+	return "(nil)";
 }
