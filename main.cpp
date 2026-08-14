936caa758bd536deafbbb27a704d87d9d37bf9dd
KSY <sye.g112@gmail.com>
2026-08-14 21:12:50 +0900

    fix: 출력 형식 깨짐 해결

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
 
 #include "KVDatabase.h"
+
+// 출력을 위한 전역 자물쇠
+std::mutex cout_mutex;
+
 
 void threadTask(KVDatabase& db, int thread_id) {
     std::string key = "user_" + std::to_string(thread_id);
 
     db.set(key, "data");
+
+    std::string result = db.get(key);
+
+    // cout 독점 시작
+    {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "[Thread " << thread_id << "] GET " << key << ": " << result << "\n"; 
+    }
+    // 자동 unlock
+
 
     std::cout << "[Thread " << thread_id << "] GET " << key << ": " << db.get(key) << "\n";
 }
