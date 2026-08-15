2b7453513b064edb1ad6dda8264e396292ef1247
KSY <sye.g112@gmail.com>
2026-08-15 15:25:21 +0900

    style: .clang-format 규칙 수정 및 전체 코드 포맷팅 적용

--- KVDatabase.h
+++ KVDatabase.h
@@ -4,17 +4,17 @@
 #include <unordered_map>
 
 class KVDatabase {
-   private:
-    std::unordered_map<std::string, std::string> store;
+ private:
+  std::unordered_map<std::string, std::string> store;
 
-    // 읽기 쓰기 락
-    // mutable 키워드는 const 함수(get) 안에서도 이 변수를 수정(lock)할 수 있게 한다.
-    // 지금 1명이 읽고 있다는 상태를 기록해야하므로 수정이 가능해야 한다
-    mutable std::shared_mutex rw_mutex;
+  // 읽기 쓰기 락
+  // mutable 키워드는 const 함수(get) 안에서도 이 변수를 수정(lock)할 수 있게 한다.
+  // 지금 1명이 읽고 있다는 상태를 기록해야하므로 수정이 가능해야 한다
+  mutable std::shared_mutex rw_mutex;
 
-   public:
-    // .h파일에는 메서드의 껍데기(프로토타일)만 선언한다.
-    void set(const std::string& key, const std::string& value);
+ public:
+  // .h파일에는 메서드의 껍데기(프로토타일)만 선언한다.
+  void set(const std::string& key, const std::string& value);
 
-    std::string get(const std::string& key) const;
+  std::string get(const std::string& key) const;
 };
--- main.cpp
+++ main.cpp
@@ -1,46 +1,46 @@
 #include <Windows.h>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
-#include <mutex>
 
 #include "KVDatabase.h"
 
 // 출력을 위한 전역 자물쇠
 std::mutex cout_mutex;
 
-
 void threadTask(KVDatabase& db, int thread_id) {
-    std::string key = "user_" + std::to_string(thread_id);
+  std::string key = "user_" + std::to_string(thread_id);
 
-    db.set(key, "data");
+  db.set(key, "data");
 
-    std::string result = db.get(key);
+  std::string result = db.get(key);
 
-    // cout 독점 시작
-    {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "[Thread " << thread_id << "] GET " << key << ": " << result << "\n";
-    }
-    // 자동 unlock
+  // cout 독점 시작
+  {
+    std::lock_guard<std::mutex> lock(cout_mutex);
+    std::cout << "[Thread " << thread_id << "] GET " << key << ": " << result
+              << "\n";
+  }
+  // 자동 unlock
 }
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
+  SetConsoleOutputCP(CP_UTF8);
 
-    std::cout << "=== Mini-Redis 서버 시작 ===\n";
-    KVDatabase db;
-    std::vector<std::thread> threads;
+  std::cout << "=== Mini-Redis 서버 시작 ===\n";
+  KVDatabase db;
+  std::vector<std::thread> threads;
 
-    for (int i = 0; i < 10; i++) {
-        threads.emplace_back(threadTask, std::ref(db), i);
-    }
+  for (int i = 0; i < 10; i++) {
+    threads.emplace_back(threadTask, std::ref(db), i);
+  }
 
-    for (auto& t : threads) {
-        t.join();
-    }
+  for (auto& t : threads) {
+    t.join();
+  }
 
-    std::cout << "=== 끝 == \n";
+  std::cout << "=== 끝 == \n";
 
-    return 0;
+  return 0;
 }
